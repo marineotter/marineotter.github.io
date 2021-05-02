4d82ac5864e506ea7aa7d17c0797009f547f335b
Yoshiyuki Harada <noreply@kawauso-lab.org>
2021-05-03 00:29:16 +0900

    Material UI 化 メニューバーのみ

--- landingpage/src/App.tsx
+++ landingpage/src/App.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
-import './App.css';
+import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
+import MenuIcon from '@material-ui/icons/Menu';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        川獺技研
-      </header>
+      <AppBar position="static">
+        <Toolbar>
+          <Typography variant="h6">
+            川獺技研 - kawauso-lab
+          </Typography>
+        </Toolbar>
+      </AppBar>
     </div>
   );
 }
