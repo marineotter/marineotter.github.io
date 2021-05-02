365c7e2a506ae5959b1df88989f37b34f7a77e89
Yoshiyuki Harada <noreply@kawauso-lab.org>
2021-05-03 00:39:51 +0900

    表示可能にした

--- landingpage/src/App.tsx
+++ landingpage/src/App.tsx
@@ -1,17 +1,37 @@
 import React from 'react';
+import { createMuiTheme } from '@material-ui/core/styles';
+import { ThemeProvider } from '@material-ui/styles';
 import { AppBar, Toolbar, IconButton, Typography, Button } from '@material-ui/core';
+import { blueGrey, orange } from '@material-ui/core/colors';
 import MenuIcon from '@material-ui/icons/Menu';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: blueGrey[50],
+      main: blueGrey[100],
+      dark: blueGrey[400],
+    },
+    secondary: {
+      light: orange[50],
+      main: orange[100],
+      dark: orange[400],
+    },
+  },
+});
 
 function App() {
   return (
     <div className="App">
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6">
-            川獺技研 - kawauso-lab
-          </Typography>
-        </Toolbar>
-      </AppBar>
+      <ThemeProvider theme={theme}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6">
+              川獺技研 - kawauso-lab
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </ThemeProvider>
     </div>
   );
 }
